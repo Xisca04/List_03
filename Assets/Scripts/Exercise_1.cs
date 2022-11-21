cd3be3efa37148572a5da285412d8d551848599b
Xisca04 <xiscacerdavicens@gmail.com>
2022-11-21 08:01:35 +0100

    ejercicio_1

--- Assets/Scripts/Exercise_1.cs
+++ Assets/Scripts/Exercise_1.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Exercise_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Consonant or vowel -- Bool
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string letter;
+
 }
